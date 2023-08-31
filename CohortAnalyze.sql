d325d65af2731a69893ff628ba3619362fa64787
BrianNgo2 <122535751+BrianNgo2@users.noreply.github.com>
2023-08-31 21:06:22 +0700

    Add files via upload

---
+++ CohortAnalyze.sql
@@ -0,0 +1,179 @@
+﻿/* Ex1. Using Database AdventureWorksDW2020, table dbo.FactInternetSales 
+
+Write a query that'll query Rention Cohort Analysis based on First time Customer Purchase in the period of Jan 2020 to Jan 2021*/ 
+
+ 
+
+Use AdventureWorksDW2020 
+
+; 
+
+  
+
+WITH  
+
+Orderlist AS ( 
+
+SELECT  
+
+CustomerKey 
+
+, OrderDate 
+
+FROM FactInternetSales 
+
+), 
+
+FPD AS ( 
+
+Select  
+
+CustomerKey 
+
+,MIN(OrderDate) AS FirstPurchaseDate 
+
+,FORMAT(MIN(OrderDate),'yyyy-MM') As FirstPurchaseMonth 
+
+From Orderlist 
+
+Group by CustomerKey 
+
+), CHI AS ( 
+
+SELECT  
+
+DISTINCT OL.CustomerKey 
+
+,FPD.FirstPurchaseMonth 
+
+,DATEDIFF(MONTH,FirstPurchaseDate,OrderDate) AS CohortInd 
+
+FROM Orderlist OL  
+
+LEFT JOIN FPD FPD ON OL.CustomerKey=FPD.CustomerKey 
+
+), 
+
+CohortPivot AS ( 
+
+SELECT  
+
+* 
+
+FROM CHI 
+
+PIVOT ( 
+
+COUNT (Customerkey) 
+
+FOR CohortIND IN ( 
+
+[0] 
+
+,[1] 
+
+,[2] 
+
+,[3] 
+
+,[4] 
+
+,[5] 
+
+,[6] 
+
+,[7] 
+
+,[8] 
+
+,[9] 
+
+,[10] 
+
+,[11] 
+
+,[12] 
+
+) ) AS  PT 
+
+) 
+
+  
+
+  
+
+SELECT  
+
+FirstPurchaseMonth 
+
+, FORMAT([0]/[0],'p') AS '0' 
+
+, FORMAT(1.0*[1]/[0],'p') AS '1' 
+
+, FORMAT(1.0*[2]/[0],'p') AS '2' 
+
+, FORMAT(1.0*[3]/[0],'p') AS '3' 
+
+, FORMAT(1.0*[4]/[0],'p') AS '4' 
+
+, FORMAT(1.0*[5]/[0],'p') AS '5' 
+
+, FORMAT(1.0*[6]/[0],'p') AS '6' 
+
+, FORMAT(1.0*[7]/[0],'p') AS '7' 
+
+, FORMAT(1.0*[8]/[0],'p') AS '8' 
+
+, FORMAT(1.0*[9]/[0],'p') AS '9' 
+
+, FORMAT(1.0*[10]/[0],'p') AS '10' 
+
+, FORMAT(1.0*[11]/[0],'p') AS '11' 
+
+, FORMAT(1.0*[12]/[0],'p') AS '12' 
+
+FROM CohortPivot 
+
+Order by 1 DESC 
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
+
+-- Task 2. Thực hiện bài toán bên dưới 
+
+ 
+
+/* Ex2. Sử dụng Database AdventureWorksFull, bảng Sales.SalesOrderHeder 
+
+Xác định các CustomerID của Khách hàng mua hàng từ 2 đơn vào 2 ngày liên tiếp 
+
+ */ 
+
+ 
+
+USE AdventureWorksFull
+
+SELECT 
+
+DISTINCT SOD.SalesOrderID 
+
+  
+
+FROM Sales.SalesOrderHeader SOD  
+
+  
+
+  
+
+JOIN Sales.SalesOrderHeader SO ON SOD.CustomerID=SO.CustomerID 
+
+  
+
+WHERE  DATEDIFF(Day,SOD.OrderDate,SO.OrderDate) = 1 
---
+++ RFManalyze-InventoryTracking.sql
@@ -0,0 +1,245 @@
+﻿-- Task 1. Thực hiện lại  bài Lab về RFM analysis 
+
+ 
+
+USE AdventureWorksDW2020 
+
+; 
+
+  
+
+WITH OrderList AS ( 
+
+SELECT  
+
+CustomerKey 
+
+,OrderDate 
+
+,SalesAmount 
+
+,SalesOrderNumber 
+
+FROM FactInternetSales 
+
+), RFM_record AS ( 
+
+SELECT  
+
+CustomerKey 
+
+, COUNT(DISTINCT SalesOrderNumber ) AS F  
+
+,SUM(SalesAmount) AS M 
+
+, DATEDIFF(DAY,MAX(ORDERDATE),(SELECT MAX (OrderDate) FROM OrderList)) AS R 
+
+FROM OrderList 
+
+Group by CustomerKey 
+
+), RFM_Score AS ( 
+
+SELECT  
+
+CustomerKey 
+
+,NTILE(4) OVER (ORDER BY R DESC) AS R_Score 
+
+,NTILE(4) OVER (ORDER BY M) AS M_Score 
+
+,NTILE(4) OVER (ORDER BY F) AS F_Score 
+
+FROM RFM_record 
+
+), RFM AS ( 
+
+SELECT  
+
+CustomerKey 
+
+,CONCAT(R_Score,F_Score,M_Score) AS RFM  
+
+FROM RFM_Score 
+
+) 
+
+SELECT  
+
+CustomerKey 
+
+,CASE 
+
+WHEN RFM LIKE '1__' THEN 'CHURNED' 
+
+WHEN RFM LIKE '[3-4][3-4][3-4]' THEN 'LOYAL' 
+
+WHEN RFM LIKE '[3-4][3-4][1-2]' THEN 'PROMISING' 
+
+WHEN RFM LIKE '_[1-2]4' THEN 'BIGSPENDER' 
+
+WHEN RFM LIKE '[3-4][1-2]_' THEN 'NEWCUSTOMER' 
+
+WHEN RFM LIKE '2__' THEN 'POTENTIALCHURNED' 
+
+END AS Cust_Seg 
+
+,RFM 
+
+FROM RFM 
+
+ 
+
+--Task 2. Tính toán các yêu cầu sau   
+
+ 
+
+/* Ex2. Từ bảng FactInventory, thống kê tổng số lượng hàng tồn kho (UnitBalance) đối với từng sản phẩm tại ngày cuối cùng của mỗi tháng.  
+
+đồng thời tổng hợp lên tất cả sản phẩm trong tháng  
+
+Kết quả trả ra gồm: Month (định dạng MM-yyyy), EndofMonthDate, EnglishProductName, TotalUnitsBalance 
+
+*/ 
+
+/* 
+
+Ý tưởng của em là : Tính ra Ngày đầu tiên hàng xuất hiện trong kho rồi cộng với chênh lệch lũy kế hàng tháng để ra inventory track  
+
+Bài làm hơi dài vì trong thực tế có thể có sản phẩm xuất hiện sau một số sản phẩm khác, nhưng trong bài này em test thử thì mọi mặt hàng đều xuất hiện cùng nhau là tháng 1-2018 
+
+ B1: Lấy ra các cột cần dùng 
+
+B2:Tìm Ngày đầu tiên xuất hiện của hàng trong inventory -> Tìm được Số lượng ngày đầu tiên trong kho 
+
+B3: Tìm chênh lệch hàng (IN-OUT) -> lũy kế chênh lệch 
+
+B4: Lấy lũy kế cuối mỗi tháng + Số lượng ban đầu 
+
+*/ 
+
+ 
+
+USE AdventureWorksDW2020 
+
+; 
+
+WITH Inv AS ( 
+
+SELECT  
+
+ProductKey 
+
+, Date 
+
+, EOMONTH(Date) AS EOM 
+
+,UnitsIn 
+
+,UnitsOut 
+
+,UnitsBalance 
+
+FROM  FactProductInventory 
+
+) 
+
+, FirstDateInv AS ( 
+
+SELECT  
+
+ProductKey 
+
+,MiN(Date) FirstDate 
+
+FROM Inv 
+
+GROUP BY ProductKey  
+
+) 
+
+, FirstDayUnitBalance AS ( 
+
+SELECT  
+
+DISTINCT I.ProductKey 
+
+,I.UnitsBalance AS FIRSTDAYUNITBALANCE 
+
+FROM Inv I 
+
+LEFT JOIN FirstDateInv FI On I.ProductKey=FI.ProductKey AND I.Date=FI.FirstDate 
+
+) 
+
+,Chenhlechthang AS ( 
+
+SELECT  
+
+DISTINCT ProductKey 
+
+,EOM 
+
+,(SUM(UnitsIn)-SUM(UnitsOut)) AS Chenh_lech_hang_thang 
+
+FROM Inv  
+
+GROUP BY PRODUCTKEY,EOM 
+
+) 
+
+SELECT 
+
+Clt.ProductKey 
+
+,FORMAT(Clt.EOM,'yyyy-MM') AS 'Month-Year' 
+
+,FI.FIRSTDAYUNITBALANCE+(SUM(Clt.Chenh_lech_hang_thang) OVER ( PARTITION BY Clt.Productkey Order by Clt.EOM )) AS INVENTORY_TRACK 
+
+FROM Chenhlechthang Clt  
+
+LEFT JOIN FirstDayUnitBalance FI ON Clt.ProductKey=FI.ProductKey 
+
+ORDER BY 1,2 
+
+ 
+
+/* Ex3. Từ bảng DimEmployee,  
+
+tính tổng thời gian dài nhất mỗi phòng ban không tuyển dụng bất cứ ai (sử dụng cột HireDate)  
+
+*/ 
+
+ 
+
+ 
+
+USE AdventureWorksDW2020 
+
+; 
+
+WITH ChenhLech AS ( 
+
+SELECT 
+
+DepartmentName 
+
+,HireDate 
+
+,LAG(HireDate) OVER (ORDER BY HireDate) AS OK 
+
+,DATEDIFF(DAY,LAG(HireDate) OVER (PARTITION BY Departmentname ORDER BY HireDate),HireDate) AS Chenhlechngay 
+
+FROM DimEmployee 
+
+) 
+
+SELECT  
+
+DepartmentName 
+
+,MAX(Chenhlechngay) AS ThoiGianKhongTuyenDaiNhat 
+
+FROM ChenhLech 
+
+Group by DepartmentName 
